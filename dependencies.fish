ed85d58e75138a863d44a4159a8a7d1b08ff6d27
EsGeh <SamuelGfroerer@googlemail.com>
2019-03-24 14:19:49 +0100

    update some dependencies. use "http" to reference repos instead of ssh.

--- dependencies.fish
+++ dependencies.fish
@@ -13,15 +13,15 @@ set dependencies \
 # 	name uri version
 set fishshell_cmd_opts \
 	fishshell-cmd-opts  \
-	git@github.com:EsGeh/fishshell-cmd-opts.git	\
+	https://github.com/EsGeh/fishshell-cmd-opts.git \
 	e446daf1741b416ecb83e4741b4cb7f99645bc11
 set structuredData \
 	structuredData \
-	git@github.com:EsGeh/structuredData.git	\
-	c46db534c105e0477145f0bc67f41986c1d11cdd \
+	https://github.com/EsGeh/structuredData.git \
+	ad27977ea4345defabc13358ec0cffd5094721b6 \
 	"scripts/init.fish --deps-dir \$DEP_DIR"
 set sdScript \
 	sdScript \
-	git@github.com:EsGeh/sdScript.git	\
-	ed944372af7777ae5defab820ed9a71e82a49796 \
+	https://github.com/EsGeh/sdScript.git \
+	92b6704725d4493f94c7beb634100d01a6d1272d \
 	"scripts/init.fish --deps-dir \$DEP_DIR"
