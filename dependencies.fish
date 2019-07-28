fd0ac1ab695c51147d735102a3410b82d6e8cae8
EsGeh <SamuelGfroerer@googlemail.com>
2019-07-28 12:52:29 +0200

    update dependencies

--- dependencies.fish
+++ dependencies.fish
@@ -14,8 +14,9 @@ set fishshell_cmd_opts \
 	fishshell-cmd-opts  \
 	https://github.com/EsGeh/fishshell-cmd-opts.git \
 	e446daf1741b416ecb83e4741b4cb7f99645bc11
+
 set structuredData \
 	structuredData \
 	https://github.com/EsGeh/structuredData.git \
-	2b9cb0d39ccd3b55850ac304aff3594329a6c8d5 \
+	4609b7ef06e6d8dd1c4ed602dda0b7ff9e1f92b8 \
 	"scripts/init.fish --deps-dir \$DEP_DIR"
